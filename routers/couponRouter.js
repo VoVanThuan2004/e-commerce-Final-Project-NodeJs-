fd08a117dce716663415d98ee6d36a0b26b60b86
Tien Dat <126091355+TDAt2k4@users.noreply.github.com>
2025-11-23 23:11:04 +0700

    couponRouter

--- routers/couponRouter.js
+++ routers/couponRouter.js
@@ -3,8 +3,12 @@ const router = express.Router();
 const auth = require("../middlewares/auth");
 const couponController = require("../controllers/couponController");
 
-router.post("/api/v1/coupons/", auth, couponController.addCoupon);
+router.post("/api/v1/coupons", auth, couponController.addCoupon);
 router.put("/api/v1/coupons/:couponId", auth, couponController.updateCoupon);
 router.delete("/api/v1/coupons/:couponId", auth, couponController.deleteCoupon);
+router.get("/api/v1/coupons", auth, couponController.getAllCoupons);
+
+// Tra cứu mã giảm giá
+router.get("/api/v1/check-coupon", couponController.checkCoupon);
 
 module.exports = router;
